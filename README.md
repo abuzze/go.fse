db2478c267bbe54244650912d6bc572126735290
Christoph Miessler <christoph.miessler@xing.com>
2021-09-18 09:11:45 +0200

    Add search features
    Signed-off-by: Christoph Miessler <christoph.miessler@xing.com>

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ a specific aircraft.
 
 ## Install
 
-Copy the binary, the config.sample.json and the airport-codes_json.json to a folder.
+Copy the binary, the config.sample.json and the airport-codes_json.json to the same folder.
 Run the binary in a terminal like iTerm (MacOS) or cmd (Windows)
 
 ## Config
@@ -15,8 +15,25 @@ Run the binary in a terminal like iTerm (MacOS) or cmd (Windows)
 You need to rename config.sample.json to config.json and edit your userkey and the
 desired aircraft type. You will find you userkey on the (FSEconomy Website)[https://server.fseconomy.net/] via
 Home > Datafeeds in the Access Key field. It looks like this 3EF7A5E5746994GF.
+The config.json must be in the some folder as the go.fse tool.
+
 The aircraft model should be the same like in the "Search Airport" dropdown menu "Airports that have this aircraft".
+
 Set terminal to cmd for Windows or to bash, if you like colors in your terminal.
+
+From and to can contain country iso codes like "US", "UK" or "DE" to narrow to the search for only a particular list of country.
+Both can be a list of multiple countries. If you want to search for all jobs that start from China or the US use this list
+
+  "from":["US","CN"],
+
+The same applies for the to list. If you don't want to restrict it, use ["all"]. Please note that the square brackets surround the list, even
+if it contains only one item.
+
+  "to":["all"],
+
+You can limit the number of jobs shown be setting the searchlimit.
+
+  "searchlimit":4
 
 ## Airport Data
 
--- main.go
+++ main.go
@@ -15,241 +15,249 @@ import (
 	"strings"
 )
 
-var userkey string
-var AircraftType string
-var Terminal string
+var config Config
 
 type AircraftItems struct {
-	XMLName  xml.Name   `xml:"AircraftItems"`
-	Aircraft []Aircraft `xml:"Aircraft"`
+  XMLName  xml.Name   `xml:"AircraftItems"`
+  Aircraft []Aircraft `xml:"Aircraft"`
 }
 
 type Aircraft struct {
-	XMLName      xml.Name `xml:"Aircraft"`
-	SerialNumber string   `xml:"SerialNumber"`
-	Location     string   `xml:"Location"`
-	NeedsRepair  string   `xml:"NeedsRepair"`
+  XMLName      xml.Name `xml:"Aircraft"`
+  SerialNumber string   `xml:"SerialNumber"`
+  Location     string   `xml:"Location"`
+  NeedsRepair  string   `xml:"NeedsRepair"`
 }
 
 type IcaoJobsFrom struct {
-	XMLName    xml.Name     `xml:"IcaoJobsFrom"`
-	Assignment []Assignment `xml:"Assignment"`
+  XMLName    xml.Name     `xml:"IcaoJobsFrom"`
+  Assignment []Assignment `xml:"Assignment"`
 }
 
 type Assignment struct {
-	XMLName        xml.Name `xml:"Assignment"`
-	Location       string   `xml:"Location"`
-	ToIcao         string   `xml:"ToIcao"`
-	Type           string   `xml:"Type"`
-	AircraftId     string   `xml:"AircraftId"`
-	Commodity      string   `xml:"Commodity"`
-	Pay            float64  `xml:"Pay"`
-	Expires        string   `xml:"Expires"`
-	ExpireDateTime string   `xml:"ExpireDateTime"`
+  XMLName        xml.Name `xml:"Assignment"`
+  Location       string   `xml:"Location"`
+  ToIcao         string   `xml:"ToIcao"`
+  Type           string   `xml:"Type"`
+  AircraftId     string   `xml:"AircraftId"`
+  Commodity      string   `xml:"Commodity"`
+  Pay            float64  `xml:"Pay"`
+  Expires        string   `xml:"Expires"`
+  ExpireDateTime string   `xml:"ExpireDateTime"`
 }
 
 type Job struct {
-	Location string
-	ToIcao   string
-	Expires  string
-	Pay      int
+  Location string
+  ToIcao   string
+  Expires  string
+  Pay      int
 }
 
 type GPS_Coordinates struct {
-	lat1 float64
-	lon1 float64
+  lat1 float64
+  lon1 float64
 }
 
 type Airport struct {
-	Ident        string `json:"ident"`
-	Name         string `json:"name"`
-	Iso_country  string `json:"iso_country"`
-	Elevation_ft string `json:"elevation_ft"`
-	Iata_code    string `json:"iata_code"`
-	Iso_region   string `json:"iso_region"`
-	Type         string `json:"type"`
-	Coordinates  string `json:"coordinates"`
+  Ident        string `json:"ident"`
+  Name         string `json:"name"`
+  Iso_country  string `json:"iso_country"`
+  Elevation_ft string `json:"elevation_ft"`
+  Iata_code    string `json:"iata_code"`
+  Iso_region   string `json:"iso_region"`
+  Type         string `json:"type"`
+  Coordinates  string `json:"coordinates"`
 }
 
 type Config struct {
-	Userkey      string `json:"userkey"`
-	Aircrafttype string `json:"aircrafttype"`
-	Terminal     string `json:"terminal"`
+  Userkey      string `json:"userkey"`
+  Aircrafttype string `json:"aircrafttype"`
+  Terminal     string `json:"terminal"`
+  From     		 []string `json:"from"`
+  To     			 []string `json:"to"`
+  Searchlimit  int `json:"searchlimit"`
 }
 
-var data []byte
 
 func main() {
-	readConfig()
-	data = readAirportData()
-	printTopJobs()
-	pauseOutput()
-
+  readConfig()
+  printTopJobs()
+  pauseOutput()
 }
 
 func pauseOutput(){
-	fmt.Print("Press 'Enter' to continue...")
+  fmt.Print("Press 'Enter' to continue...")
   bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
 
 func readConfig() {
-	config, err := ioutil.ReadFile("./config.json")
-	if err != nil {
-		fmt.Print(err)
-	}
-	var Config Config
-	json.Unmarshal(config, &Config)
-	userkey = Config.Userkey
-	Terminal = Config.Terminal
-	AircraftType = strings.Replace(Config.Aircrafttype, " ", "%20", -1)
+  configfile, err := ioutil.ReadFile("./config.json")
+  if err != nil {
+    fmt.Print(err)
+  }
+  json.Unmarshal(configfile, &config)
+  config.Aircrafttype = strings.Replace(config.Aircrafttype, " ", "%20", -1)
 }
 
 func convertGPSstring(Coordinates string) GPS_Coordinates {
-	var GPS_Coordinates GPS_Coordinates
-	coords := strings.Split(Coordinates, ",")
-	if lat1, err := strconv.ParseFloat(strings.Trim(coords[0], " "), 64); err == nil {
-		GPS_Coordinates.lat1 = lat1
-	}
-	if lon1, err := strconv.ParseFloat(strings.Trim(coords[1], " "), 64); err == nil {
-		GPS_Coordinates.lon1 = lon1
-	}
-	return GPS_Coordinates
+  var GPS_Coordinates GPS_Coordinates
+  coords := strings.Split(Coordinates, ",")
+  if lat1, err := strconv.ParseFloat(strings.Trim(coords[0], " "), 64); err == nil {
+    GPS_Coordinates.lat1 = lat1
+  }
+  if lon1, err := strconv.ParseFloat(strings.Trim(coords[1], " "), 64); err == nil {
+    GPS_Coordinates.lon1 = lon1
+  }
+  return GPS_Coordinates
 }
 
 func convertKMtoNM(KM int) int {
-	return int(float64(KM) * 0.5399565)
+  return int(float64(KM) * 0.5399565)
 }
 func degreesToRadians(degress float64) float64 {
-	return degress * math.Pi / 180
+  return degress * math.Pi / 180
 }
 
 func distanceInKmBetweenEarthCoordinates(lat1 float64, lon1 float64, lat2 float64, lon2 float64) int {
-	var earthRadiusKm = 6371.0
-	var dLat = degreesToRadians(lat2 - lat1)
-	var dLon = degreesToRadians(lon2 - lon1)
-	lat1 = degreesToRadians(lat1)
-	lat2 = degreesToRadians(lat2)
-	var a = math.Sin(dLat/2)*math.Sin(dLat/2) + math.Sin(dLon/2)*math.Sin(dLon/2)*math.Cos(lat1)*math.Cos(lat2)
-	var c = 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
-	return int(earthRadiusKm * c)
+  var earthRadiusKm = 6371.0
+  var dLat = degreesToRadians(lat2 - lat1)
+  var dLon = degreesToRadians(lon2 - lon1)
+  lat1 = degreesToRadians(lat1)
+  lat2 = degreesToRadians(lat2)
+  var a = math.Sin(dLat/2)*math.Sin(dLat/2) + math.Sin(dLon/2)*math.Sin(dLon/2)*math.Cos(lat1)*math.Cos(lat2)
+  var c = 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+  return int(earthRadiusKm * c)
 }
 
 func readAirportData() []byte {
-	data, err := ioutil.ReadFile("./airport-codes_json.json")
-	if err != nil {
-		fmt.Print(err)
-	}
-	return data
+  data, err := ioutil.ReadFile("./airport-codes_json.json")
+  if err != nil {
+    fmt.Print(err)
+  }
+  return data
 }
 
 func getAirportData(ICAO string) Airport {
-	var AirportInfo Airport
-	var obj []Airport
-	json.Unmarshal(data, &obj)
-	for i := 0; i < len(obj); i++ {
-		if ICAO == obj[i].Ident {
-			AirportInfo = obj[i]
-		}
-	}
-	return AirportInfo
+  var AirportInfo Airport
+  var obj []Airport
+  json.Unmarshal(readAirportData(), &obj)
+  for i := 0; i < len(obj); i++ {
+    if ICAO == obj[i].Ident {
+      AirportInfo = obj[i]
+    }
+  }
+  return AirportInfo
 }
 
 func getAircrafts() AircraftItems {
 
-	Aircraftresponse, err := http.Get("https://server.fseconomy.net/data?userkey=" + userkey + "&format=xml&query=aircraft&search=makemodel&makemodel=" + AircraftType)
+  Aircraftresponse, err := http.Get("https://server.fseconomy.net/data?userkey=" + config.Userkey + "&format=xml&query=aircraft&search=makemodel&makemodel=" + config.Aircrafttype)
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
+  if err != nil {
+    fmt.Print(err.Error())
+    os.Exit(1)
+  }
 
-	responseData, err := ioutil.ReadAll(Aircraftresponse.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+  responseData, err := ioutil.ReadAll(Aircraftresponse.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	var AircraftItem AircraftItems
-	xml.Unmarshal(responseData, &AircraftItem)
-	if Terminal == "bash" {
-		fmt.Println("Found " + "\033[31m" + strconv.Itoa(len(AircraftItem.Aircraft)) + "\033[0m Aircrafts of type " + strings.Replace(AircraftType, "%20", "", -1))
-	} else {
-		fmt.Println("Found " + strconv.Itoa(len(AircraftItem.Aircraft)) + " Aircrafts of type " + strings.Replace(AircraftType, "%20", "", -1))
-	}
-	return AircraftItem
+  var AircraftItem AircraftItems
+  xml.Unmarshal(responseData, &AircraftItem)
+  if config.Terminal == "bash" {
+    fmt.Println("Found " + "\033[31m" + strconv.Itoa(len(AircraftItem.Aircraft)) + "\033[0m Aircrafts of type " + strings.Replace(config.Aircrafttype, "%20", "", -1))
+  } else {
+    fmt.Println("Found " + strconv.Itoa(len(AircraftItem.Aircraft)) + " Aircrafts of type " + strings.Replace(config.Aircrafttype, "%20", "", -1))
+  }
+  return AircraftItem
 }
 
 func getAssignment(AircraftItem AircraftItems) IcaoJobsFrom {
-	var Locationstr string
-	var IcaoJobsFromList IcaoJobsFrom
-	for i := 0; i < len(AircraftItem.Aircraft); i++ {
-		// concat all airports
-		if AircraftItem.Aircraft[i].Location != "In Flight" {
-			if i == 0 {
-				Locationstr = AircraftItem.Aircraft[i].Location
-			} else {
-				Locationstr = Locationstr + "-" + AircraftItem.Aircraft[i].Location
-			}
+  var Locationstr string
+  var IcaoJobsFromList IcaoJobsFrom
+  for i := 0; i < len(AircraftItem.Aircraft); i++ {
+    // concat all airports
+    if AircraftItem.Aircraft[i].Location != "In Flight" {
+         Locationstr = Locationstr + "-" + AircraftItem.Aircraft[i].Location
+    }
+  }
+  // remove a leading -
+  if Locationstr[0] == '-' {
+    Locationstr = Locationstr[1:]
+  }
+  // get possible assignments on all airports, that have our aircraft type
+  Assignmentresponse, err := http.Get("https://server.fseconomy.net/data?userkey=" + config.Userkey + "&format=xml&query=icao&search=jobsfrom&icaos=" + Locationstr)
+  if err != nil {
+    fmt.Print(err.Error())
+    os.Exit(1)
+  }
+  responseData, err := ioutil.ReadAll(Assignmentresponse.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+  xml.Unmarshal(responseData, &IcaoJobsFromList)
+  return IcaoJobsFromList
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
 		}
 	}
-	// remove a leading -
-	if Locationstr[0] == '-' {
-		Locationstr = Locationstr[1:]
-	}
-	// get possible assignments on all airports, that have our aircraft type
-	Assignmentresponse, err := http.Get("https://server.fseconomy.net/data?userkey=" + userkey + "&format=xml&query=icao&search=jobsfrom&icaos=" + Locationstr)
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	responseData, err := ioutil.ReadAll(Assignmentresponse.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	xml.Unmarshal(responseData, &IcaoJobsFromList)
-	return IcaoJobsFromList
+
+	return false
 }
 
 func printTopJobs() {
-	var Aircrafts AircraftItems = getAircrafts()
-	var IcaoJobsFromList IcaoJobsFrom = getAssignment(Aircrafts)
-	var myList []Job
-	// var Joblists [] Joblist
-	for i := 0; i < len(IcaoJobsFromList.Assignment); i++ {
-		if IcaoJobsFromList.Assignment[i].AircraftId != "0" {
-			for j := 0; j < len(Aircrafts.Aircraft); j++ {
-				// Only add Planes, that we searched
-				if Aircrafts.Aircraft[j].SerialNumber == IcaoJobsFromList.Assignment[i].AircraftId {
-					myList = append(myList, Job{
-						Location: IcaoJobsFromList.Assignment[i].Location,
-						Pay:      int(IcaoJobsFromList.Assignment[i].Pay),
-						ToIcao:   IcaoJobsFromList.Assignment[i].ToIcao,
-						Expires:  IcaoJobsFromList.Assignment[i].Expires,
-					})
-				}
-			}
-		}
-	}
-	sort.SliceStable(myList, func(i, j int) bool {
-		return myList[i].Pay > myList[j].Pay
-	})
-	for i := 0; i < len(myList) && i < 10; i++ {
-		var Airport Airport = getAirportData(myList[i].Location)
-		var TravelDistance int = calculateDistanceNM(myList[i].Location, myList[i].ToIcao)
-		var AirportInfoStr string = Airport.Ident + ", " + Airport.Name + ", " + Airport.Iso_country + ", " + Airport.Type
-		if Terminal == "bash" {
-			fmt.Println("\033[32m" + myList[i].Location + " > " + myList[i].ToIcao + "\033[0m" + ", " + "\033[33m" + strconv.Itoa(TravelDistance) + " NM" + "\033[0m" + ", Expires in: " + myList[i].Expires + " , " + "\033[35m" + strconv.Itoa(myList[i].Pay) + "$" + "\033[0m" + " : " + AirportInfoStr)
-		} else {
-			fmt.Println(myList[i].Location + " > " + myList[i].ToIcao + ", " + strconv.Itoa(TravelDistance) + " NM , Expires in: " + myList[i].Expires + " , " + strconv.Itoa(myList[i].Pay) + "$" + " : " + AirportInfoStr)
-		}
-
-	}
+  var Aircrafts AircraftItems = getAircrafts()
+  var IcaoJobsFromList IcaoJobsFrom = getAssignment(Aircrafts)
+  var myList []Job
+  var foundJobs = 0
+  for i := 0; i < len(IcaoJobsFromList.Assignment); i++ {
+    if IcaoJobsFromList.Assignment[i].AircraftId != "0" {
+      for j := 0; j < len(Aircrafts.Aircraft); j++ {
+        // Only add Planes, that we searched
+        if Aircrafts.Aircraft[j].SerialNumber == IcaoJobsFromList.Assignment[i].AircraftId  {
+          var FromAirport Airport = getAirportData(IcaoJobsFromList.Assignment[i].Location)
+          var ToAirport Airport = getAirportData(IcaoJobsFromList.Assignment[i].ToIcao)
+          if (contains(config.From, FromAirport.Iso_country) || contains(config.From,"all") ) && (contains(config.To,ToAirport.Iso_country) || contains(config.To,"all") ) {
+            myList = append(myList, Job{
+              Location: IcaoJobsFromList.Assignment[i].Location,
+              Pay:      int(IcaoJobsFromList.Assignment[i].Pay),
+              ToIcao:   IcaoJobsFromList.Assignment[i].ToIcao,
+              Expires:  IcaoJobsFromList.Assignment[i].Expires,
+            })
+            foundJobs++
+            if foundJobs >= config.Searchlimit {
+              break
+            }
+          }
+        }
+      }
+    }
+  }
+  sort.SliceStable(myList, func(i, j int) bool {
+    return myList[i].Pay > myList[j].Pay
+  })
+  for i := 0; i < len(myList) && i < config.Searchlimit; i++ {
+    var Airport Airport = getAirportData(myList[i].Location)
+    var TravelDistance int = calculateDistanceNM(myList[i].Location, myList[i].ToIcao)
+    var AirportInfoStr string = Airport.Ident + ", " + Airport.Name + ", " + Airport.Iso_country + ", " + Airport.Type
+    if config.Terminal == "bash" {
+      fmt.Println("\033[32m" + myList[i].Location + " > " + myList[i].ToIcao + "\033[0m" + ", " + "\033[33m" + strconv.Itoa(TravelDistance) + " NM" + "\033[0m" + ", Expires in: " + myList[i].Expires + " , " + "\033[35m" + strconv.Itoa(myList[i].Pay) + "$" + "\033[0m" + " : " + AirportInfoStr)
+    } else {
+      fmt.Println(myList[i].Location + " > " + myList[i].ToIcao + ", " + strconv.Itoa(TravelDistance) + " NM , Expires in: " + myList[i].Expires + " , " + strconv.Itoa(myList[i].Pay) + "$" + " : " + AirportInfoStr)
+    }
+  }
 }
 
 func calculateDistanceNM(FromICAO string, ToICAO string) int {
-	var FROM GPS_Coordinates
-	var TO GPS_Coordinates
-	FROM = convertGPSstring(getAirportData(FromICAO).Coordinates)
-	TO = convertGPSstring(getAirportData(ToICAO).Coordinates)
-	var distance int = distanceInKmBetweenEarthCoordinates(FROM.lat1, FROM.lon1, TO.lat1, TO.lon1)
-	return convertKMtoNM(distance)
+  var FROM GPS_Coordinates
+  var TO GPS_Coordinates
+  FROM = convertGPSstring(getAirportData(FromICAO).Coordinates)
+  TO = convertGPSstring(getAirportData(ToICAO).Coordinates)
+  var distance int = distanceInKmBetweenEarthCoordinates(FROM.lat1, FROM.lon1, TO.lat1, TO.lon1)
+  return convertKMtoNM(distance)
 }
